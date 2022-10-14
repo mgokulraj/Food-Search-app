b3ef625fb0d7a4de54419774d3f1259c258bca29
mgokulraj <mgokulraj.uk@gmail.com>
2022-10-14 18:29:22 +0100

    Final

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '1ead4f81-5caf-468b-ac55-007b66a96c9e';
+export const MODAL_CLOSE_SEC = 2.5;
